2adcafd32c297b2f992ffbeeb8d101b45146d506
Kevin Jung <kev.jung.ii@gmail.com>
2021-01-13 19:58:04 -0500

    commit tree 2c2d4b85ab8dd5e8156ce64f67ccbbaf9f1011cf

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) { |dwarf, index| puts "#{index}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect { |call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+# def find_the_cheese(ingredients)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   cheese_types.each do |cheese|
+#     return cheese if ingredients.include?(cheese)
+#   end
+#   nil
+# end
+
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.find { |ingredient| cheese_types.include?(ingredient) }
 end
+
+cheese = find_the_cheese(["camembert", "other ingredient", "cheddar"])
+puts cheese
